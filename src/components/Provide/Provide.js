aae1ac47856f4924004ee1196bbc0b6e988569ed
rick <edcrfvlily5@gmail.com>
2022-02-23 18:26:40 +0800

    [modify]調整服務項目樣式

--- src/components/Provide/Provide.js
+++ src/components/Provide/Provide.js
@@ -4,11 +4,62 @@ const Provide = () => {
   return (
     <div className={classes.pageSection}>
       <div className={classes.containerL}>
-        <div>提供服務</div>
+        <div className={classes.subject}>提供服務</div>
         <ul className={classes.ulCard}>
-          <li className={classes.liCard}>RWD 網站切版 800起/頁</li>
-          <li className={classes.liCard}>RESTful API 設計 2000起</li>
-          <li className={classes.liCard}>RESTful API 介接 1000 起</li>
+          <li className={classes.liCard}>
+            <p>RWD 網站切版</p>
+            <p>NT$ 800 </p>
+            <p>起/頁</p>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+            <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              className={"button w-button"}
+              href={"https://www.facebook.com/PetBan.Studio"}
+            >
+              馬上詢價
+            </a>
+          </li>
+          <li className={classes.liCard}>
+            <p>RESTful API 設計</p>
+            <p>NT$ 2000</p>
+            <p>起</p>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+            <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              className={"button w-button"}
+              href={"https://www.facebook.com/PetBan.Studio"}
+            >
+              馬上詢價
+            </a>
+          </li>
+          <li className={classes.liCard}>
+            <p>RESTful API 介接</p>
+            <p>NT$ 1000 </p>
+            <p>起</p>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+            <a
+              target={"_blank"}
+              rel={"noreferrer"}
+              className={"button w-button"}
+              href={"https://www.facebook.com/PetBan.Studio"}
+            >
+              馬上詢價
+            </a>
+          </li>
         </ul>
       </div>
     </div>
