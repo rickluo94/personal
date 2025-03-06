a05d94b251d826e1a709581b7d4b4a88db399d99
Rick Luo <edcrfvlily5@gmail.com>
2025-03-06 19:20:14 +0800

    [modify] 新東東ＡＩ摘要

--- src/App.js
+++ src/App.js
@@ -18,31 +18,45 @@ function App() {
           <a
             target={"_blank"}
             rel={"noreferrer"}
-            href={"https://petban.sf39.app/"}
+            href={"https://chatgpt.com/share/67c96739-80e4-800e-a65c-eae2a6648f34"}
             className={"logo-button text-accent"}
           >
-            Rick CV
+            AI 摘要
           </a>
         </div>
         <div className={"container-l"}>
           <div className={"intro-text"}>
             <h1 className={"fs-700 an-h1"}>
               學無止境{"  "}朝夢想邁進
-              <br />
+              <br/>
               你好我是{"  "}
               <span className={"ff-sans-cond text-accent"}>羅亘佑！</span>
             </h1>
-            <p className={"intro-p an-intro-p"}>
-              學術成就：大學畢業專題榮獲全系第二名， 並成功申請科技部大專學生研究計畫。
-              <br />
-              早期工作經驗：服兵役前，曾在成大創新數位內容研究中心擔任產品開發測試人員。
-              <br />
-              多元職涯：出社會後，最初擔任電競直播人員，實現個人電競夢想。
-              <br />
-              隨後轉任 MIS工程師，負責Zabbix監控系統的建置與維護，並開發了一套遠端監控電腦系統（C++）。
-              <br />
-              專精領域：後來在鈞耀科技擔任軟體工程師，主要負責自動化倉儲專案，開發新功能並重構WMS系統，同時參與研發租借籃球櫃。
-            </p>
+            <div className={"intro-p an-intro-p"}>
+              <p>學術成就：</p>
+              <ul>
+                <li>大學畢業專題全系第二名</li>
+                <li>機械輔具（科技部大專學生研究計畫）</li>
+              </ul>
+              <p>早期經驗：</p>
+              <ul>
+                <li>成大創新產學，產品開發測試人員。</li>
+              </ul>
+              <p>多元職涯：</p>
+              <ul>
+                <li>電競直播人員</li>
+                <li>Zabbix監控系統建置與維護</li>
+                <li>遠端監控系統開發</li>
+              </ul>
+              <p>專精領域：</p>
+              <ul>
+                <li>網站內容管理系統 CMS</li>
+                <li>鈑金加工 ERP</li>
+                <li>自動化倉儲 ERP</li>
+                <li>開發WMS新功能，優化擴充性</li>
+                <li>租借籃球櫃研發</li>
+              </ul>
+            </div>
             <div className={"usp-wrapper"}>
               <div className={"usp-item"}>
                 <div className={"check-mark text-white"}>✓</div>
